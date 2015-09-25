3622feb9cb25259be39c709184ecfaad735ca9ac
Administrator <Administrator@XTC-20150413BBO>
2015-09-25 18:09:32 +0800

    心中freemarker测试

---
+++ commonsframework-demo-freemarker/src/main/webapp/error/401.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<c:set var="MOBILE_APP" value='<%=cn.singno.commonsframework.constant.SystemConst.ClientType.MOBILE_APP%>'></c:set>
+<c:set var="MOBILE_WX" value='<%=cn.singno.commonsframework.constant.SystemConst.ClientType.MOBILE_WX%>'></c:set>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>错误页面</title>
+</head>
+<body> 
+	<c:choose>
+		<c:when test="${ClientType==MOBILE_APP}">
+			401 error</br>
+			请求需要验证用户登录</br>
+			原生app客户端
+		</c:when>
+		<c:when test="${ClientType==MOBILE_WX}">
+			401 error</br>
+			请求需要验证用户登录</br>
+			微信客户端
+		</c:when>
+		<c:otherwise>
+			401 error</br>
+			请求需要验证用户登录</br>
+			默认客户端
+		</c:otherwise>
+	</c:choose>
+</body>
+</html>
--- commonsframework-demo2/src/main/java/cn/singno/commonsframework/interceptor/RequestRecordInterceptor.java
+++ commonsframework-demo2/src/main/java/cn/singno/commonsframework/interceptor/RequestRecordInterceptor.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.core.NamedThreadLocal;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,12 +23,20 @@ public class RequestRecordInterceptor  implements HandlerInterceptor
 	 */
 	private static final Logger logger = Logger.getLogger(RequestRecordInterceptor.class);
 	
+	private NamedThreadLocal<Long>  startTimeThreadLocal  = new NamedThreadLocal<Long>("StopWatch-StartTime"); 
+	
+	private long slowFlag = 500L;// 慢请求标准
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception
 	{
 		logger.debug("============================ 请求记录拦截器  ================================");
-		System.out.println("uri:     " + request.getRequestURI());
-		System.out.println("params:     " + JSON.toJSONString(request.getParameterMap()));
+		
+		long beginTime = System.currentTimeMillis();// 开始时间  
+        startTimeThreadLocal.set(beginTime);// 线程绑定变量（该数据只有当前请求的线程可见）
+        
+		logger.info("uri:     " + request.getRequestURI());
+		logger.info("params:     " + JSON.toJSONString(request.getParameterMap()));
 		
 		return true; 
 	}
@@ -42,7 +51,13 @@ public class RequestRecordInterceptor  implements HandlerInterceptor
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception
 	{
-		// TODO Auto-generated method stub
-
+		long endTime = System.currentTimeMillis();// 结束时间  
+        long beginTime = startTimeThreadLocal.get();// 得到线程绑定的局部变量（开始时间）  
+        long consumeTime = endTime - beginTime;// 消耗的时间  
+        if(consumeTime > slowFlag) {  
+            //TODO 记录到日志文件  
+        	
+        	logger.info(String.format("%s consume %d millis", request.getRequestURI(), consumeTime)); 
+        }
 	}
 }
