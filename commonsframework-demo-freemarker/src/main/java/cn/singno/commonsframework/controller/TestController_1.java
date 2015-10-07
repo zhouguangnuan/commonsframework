854fdb8071d2bbff4f865ce92386d5605bffda95
singno_java <zhouguangnuan@yeah.net>
2015-10-07 22:57:32 +0800

    sssssss

--- commonsframework-demo-freemarker/src/main/java/cn/singno/commonsframework/controller/TestController_1.java
+++ commonsframework-demo-freemarker/src/main/java/cn/singno/commonsframework/controller/TestController_1.java
@@ -2,10 +2,8 @@ package cn.singno.commonsframework.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 通用接口
