592bce9d5e6f261394ef8ded9ad67f0562489f34
singno <zhouguangnuan@yeah.net>
2016-02-02 09:59:59 +0800

    1111

--- commonsframework-base-utils/src/main/java/cn/singno/commonsframework/utils/CryptoUtils.java
+++ commonsframework-base-utils/src/main/java/cn/singno/commonsframework/utils/CryptoUtils.java
@@ -47,9 +47,9 @@ public class CryptoUtils
 		public class HASH
 		{
 			public static final String SHA_1 			= 		"SHA-1";
-			public static final String SHA_256 			= 		"SHA-256";
+			public static final String SHA_256 		= 		"SHA-256";
 			public static final String SHA_384			= 		"SHA-384";
-			public static final String SHA_512 			= 		"SHA-512";
+			public static final String SHA_512 		= 		"SHA-512";
 			public static final String MD2 			= 		"MD2";
 			public static final String MD5 			= 		"MD5";
 		}
