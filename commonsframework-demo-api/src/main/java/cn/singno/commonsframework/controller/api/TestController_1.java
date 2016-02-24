f502b9e776c06620d752022ae1877d41982c57b5
singno_java <zhouguangnuan@yeah.net>
2016-02-24 22:50:09 +0800

    1111

--- commonsframework-demo-api/src/main/java/cn/singno/commonsframework/controller/api/TestController_1.java
+++ commonsframework-demo-api/src/main/java/cn/singno/commonsframework/controller/api/TestController_1.java
@@ -1,10 +1,13 @@
 package cn.singno.commonsframework.controller.api;
 
+import java.io.IOException;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,7 +41,7 @@ public class TestController_1
 	
 	@RequestMapping(value="/test2/{page}/data", method=RequestMethod.GET)
 	@ResponseBody
-	public Object test3(@PathVariable String page)
+	public Object test2(@PathVariable String page)
 	{
 //		System.out.println("执行了：/test2/" + page + "/data");
 		JsonResult jsonResult = new JsonResult(DefaultDescribableEnum.SUCCESS);
@@ -46,5 +49,15 @@ public class TestController_1
                 data.put("page", page);
                 jsonResult.setData(data);
 	        return jsonResult;
+	}
+	
+	@RequestMapping(value="/test3", method=RequestMethod.GET)
+	@ResponseBody
+	public Object test3(HttpServletRequest request) throws IOException
+	{
+		String rootPath = request.getSession().getServletContext().getRealPath("/") ;
+		String emailPath = rootPath + "WEB-INF/classes/email.properties";
+		String content = FileUtils.readFileToString(FileUtils.getFile(emailPath), Charsets.UTF_8);
+	        return content;
 	}
 }
